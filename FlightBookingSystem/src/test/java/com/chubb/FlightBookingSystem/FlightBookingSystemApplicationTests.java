934a4794c1ad0d35fd8a90591b1cb1d83774de62
Ishika <22052461@kiit.ac.in>
2025-11-16 21:57:36 +0530

    Initialized the spring boot flightbookingsystem

---
+++ FlightBookingSystem/src/test/java/com/chubb/FlightBookingSystem/FlightBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chubb.FlightBookingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
